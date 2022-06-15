5690073a9f6f12f3af2427b46538744dfea0e6ab
Jerridd-S <105670541+Jerridd-S@users.noreply.github.com>
2022-06-14 20:17:59 -0500

    Add files via upload

--- left_menu_reports.php
+++ left_menu_reports.php
@@ -1,40 +1,28 @@
 <div id="menu-left">
 
-<a href="scanner_releases.php">
-  	<div <?php if($left_selected == "RELEASES")
+<a href="reports_actors.php">
+  	<div <?php if($left_selected == "ACTORS")
   	{ echo 'class="menu-left-current-page"'; } ?>>
   	<img src="./images/releases.png">
-  	<br/>Releases<br/></div>
+  	<br/>Actors<br/></div>
   </a>
 
 
-  <a href="scanner_warnings.php">
-  	<div <?php if($left_selected == "WARNINGS")
+  <a href="reports_actors_actresses.php">
+  	<div <?php if($left_selected == "ACTORS/ACTRESSES")
   	{ echo 'class="menu-left-current-page"'; } ?>>
   	<img src="./images/warnings.png">
-  	<br/>Warnings<br/></div>
+  	<br/>Actors/Actresses<br/></div>
   </a>
 
-  <a href = "scanner_errors.php">
-  	<div <?php if($left_selected == "ERRORS")
+  <a href = "reports_lyricists.php">
+  	<div <?php if($left_selected == "LYRICIST")
   	{ echo 'class="menu-left-current-page"'; } ?>>
   	<img src="./images/errors.png">
-  	<br/>Errors<br/></div>
+  	<br/>Lyricist<br/></div>
   </a>
 
-  <a href = "scanner_not_found.php">
-    <div <?php if($left_selected == "NOTFOUND")
-    { echo 'class="menu-left-current-page"'; } ?>>
-    <img src="./images/not_found.png">
-    <br/>NotFound<br/></div>
-  </a>
-
-  <a href = "scanner_goofy.php">
-  	<div <?php if($left_selected == "GOOFY")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="./images/goofy.png">
-  	<br/>Goofy<br/></div>
-  </a>
+  
 
 
 </div>
